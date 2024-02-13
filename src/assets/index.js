ebe35649e05cb2e04137dd6be4fde3bbfc035177
Mubeen202 <mubeensheikh202@gmail.com>
2024-02-13 02:50:09 -0800

    changes
    changes with real application

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo/logo.png";
+import logo from "./logo.svg";
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -6,60 +6,29 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import react from "./react.svg"
-import node from "./nodejs.svg"
-import aws from "./aws.svg"
-import docker from "./docker.svg"
-import jenkins from "./jenkins.svg"
-import kubernetes from "./kubernetes.svg"
-import python_django from "./django.svg"
-import mongo_db from "./mongodb.svg"
-import mysql from "./mysql.svg"
-import angular_js from "./angular.svg"
+
 import css from "./tech/css.png";
+import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html1.png";
-import mysql_image from "./tech/mysql.png";
-import sql from "./tech/sql.png";
-import drf from "./tech/drf.png";
-import postman from "./tech/postman.png";
-import flow from "./tech/flow.png";
-import near from "./tech/near.png";
-import haqana from "./company/haqana.png"
-
+import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
 import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import django from "./tech/django.png";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import developer from "./developer.svg"
+
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-import troon from "./company/troontech.jfif";
-import ncri from "./company/ncri.png";
-import it_routes from "./company/itRoutes.jpg";
-
 
 import carrent from "./carrent.png";
-import link from "./link.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-
-//projects Images
-import bopayment from "./projects/bopayment.png";
-import fortra from "./projects/fortra.png";
-import metaverse from "./projects/metaverse.png";
-import bamboo from "./projects/bamboo.png";
-import lagravate from "./projects/lagravate.png";
-import time_tables from "./projects/time_tables.png";
-
 
 export {
   logo,
@@ -72,9 +41,6 @@ export {
   close,
   css,
   docker,
-  aws,
-  kubernetes,
-  jenkins,
   figma,
   git,
   html,
@@ -90,41 +56,7 @@ export {
   shopify,
   starbucks,
   tesla,
-  react,
-  node,
-  angular_js,
-  python_django,
-  mongo_db,
-  mysql,
   carrent,
   jobit,
   tripguide,
-  developer,
-  link,
-  //companies
-  troon,
-  ncri,
-  it_routes,
-  haqana,
-  //projects images
-  bopayment,
-  fortra,
-  metaverse,
-  bamboo,
-  lagravate,
-  time_tables,
-  //tech
-  django,
-  mysql_image,
-  sql,
-  drf,
-  postman,
-  near,
-  flow,
-
-
-
-
-
-
 };
